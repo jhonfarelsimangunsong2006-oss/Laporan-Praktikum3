232da86796d7a8ca6af847a887aefceca0485feb
jhonfarelsimangunsong2006-oss <jhonfarelsimangunsong2006@mail.ugm.ac.id>
2025-09-23 07:41:17 +0700

    Update Akivitas 1.cpp

--- Akivitas 1.cpp
+++ Akivitas 1.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
-
-
+int A=15, B=4, temp;
+	cout << "Nilai A sebelum ditukar: " << A << ", "<< "Nilai B sebelum ditukar: " << B << endl;
+	temp = A;
+	A = B;
+	B = temp;
+	cout << "Nilai A setelah ditukar: " << A << ", " << "Nilai B setelah ditukar: " << B;
+	return 0;
 }
+
